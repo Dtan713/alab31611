0aa5c701edcd649e65f7436cda1d015c5d93b9c8
Dtan713 <s.donaldtan@gmail.com>
2024-05-28 15:11:40 -0400

    adding part 2

--- alab316.js
+++ alab316.js
@@ -1,6 +1,4 @@
-import "./alab316.css";
-import styling from "./alab316.css";
-console.log(styling);
+
 
 const mainEl = document.getElementById("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
@@ -8,3 +6,10 @@ const header = document.createElement("h1");
 header.innerHTML = "DOM Manipulation";
 mainEl.append(header);
 mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var( --top-menu-bg)";
+topMenuEl.classList.add("flex_around");
+
+console.log(topMenuEl);
